457d6bf26400929b92997a87be1e60534a6d85f9
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-06 12:27:31 -0500

    update

--- EjerciciosSemana3/src/Main.java
+++ EjerciciosSemana3/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main (String args []) {
         long documento;
         int tipo, N;
-        double comision, valventas, total=0;
+        double valventas, total=0;
         Scanner scn = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#,###.##");
 
@@ -18,7 +18,7 @@ public class Main {
             documento = Long.parseLong(scn.nextLine());
             System.out.print("VENDEDOR " + (i+1) + ": Ingrese el tipo de vendedor (1 = puerta a puerta // 2 = telemarketing): ");
             tipo = Integer.parseInt(scn.nextLine());
-            System.out.print("VENDEDOR " + (i+1) + "Ingrese el valor de las ventas del mes: $");
+            System.out.print("VENDEDOR " + (i+1) + ": Ingrese el valor de las ventas del mes: $");
             valventas = Double.parseDouble(scn.nextLine());
 
             Vendedor vendedor = new Vendedor(documento, tipo, valventas);
