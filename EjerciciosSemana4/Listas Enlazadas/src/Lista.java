189c5489d53a53b3704e42b075d9a0bb5405a401
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-11 10:41:53 -0500

    up

--- EjerciciosSemana4/Listas Enlazadas/src/Lista.java
+++ EjerciciosSemana4/Listas Enlazadas/src/Lista.java
@@ -1,2 +1,35 @@
 public class Lista {
+    public int tamano;
+    public Nodo cabeza;
+    public Nodo cola;
+
+    public Lista() {
+        this.cabeza = null;
+        this.cola = null;
+        this.tamano = 0;
+    }
+
+    public Lista(Object o){
+        this.tamano = 1;
+        Nodo nuevoNodo = new Nodo(o);
+        this.cabeza=nuevoNodo;
+        this.cola = nuevoNodo;
+    }
+
+    public boolean estaVacio(){
+        if (cabeza==null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void agregar(Object o){
+        Nodo nuevoNodo = new Nodo(o);
+        if(estaVacio()){
+            this.cabeza = nuevoNodo;
+        } else {
+            this.cola.siguiente = nuevoNodo;
+        }
+    }
 }
